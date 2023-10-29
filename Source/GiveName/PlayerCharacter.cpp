55495b48e8603232c43f32750b103727b69fea9a
AbertayFraxle <91606018+AbertayFraxle@users.noreply.github.com>
2023-10-29 17:52:58 +0000

    day 1 complete

--- Source/GiveName/PlayerCharacter.cpp
+++ Source/GiveName/PlayerCharacter.cpp
@@ -118,6 +118,12 @@ void APlayerCharacter::Interact(const FInputActionValue& Value) {
 
 			interact->InteractedWith();
 		}
+
+		if (hitActor->ActorHasTag("Hurt")) {
+
+			RedVision();
+
+		}
 	}
 }
 
