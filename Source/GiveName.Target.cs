0d1a3d1a00890a5b5f8ccbfd65b253eb5b85c0f5
AbertayFraxle <91606018+profraxle@users.noreply.github.com>
2025-02-12 19:22:22 +0000

    dixed a few things

--- Source/GiveName.Target.cs
+++ Source/GiveName.Target.cs
@@ -9,7 +9,7 @@ public class GiveNameTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_2;
 		ExtraModuleNames.Add("GiveName");
 	}
 }
